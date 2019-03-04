152417a6c2148a6fdc348060459cc1ffd2428ea5
spinon187 <spinon187@gmail.com>
2019-03-04 16:30:47 -0600

    testing I mvp, I think I did this right

--- items/enhancer.js
+++ items/enhancer.js
@@ -5,24 +5,60 @@ module.exports = {
 }
 
 function success(item) {
-    if(
-        (item.type === 'armor' && item.level < 6)
-        ||
-        (item.type === 'weapon' && item.level < 8)
-    ){
-        item.level = (item.level + 1);
-        item.displayName = `[+${item.level}] ${item.name}`;
-        return item;
-    } 
+    item.enhancement = (item.enhancement + 1);
+    if (item.enhancement < 16){
+        item.displayName = `[+${item.enhancement}] ${item.name}`
+    }
+    else if (item.enhancement === 16){
+        item.displayName = `[PRI] ${item.name}` 
+    }
+    else if (item.enhancement === 17){
+        item.displayName = `[DUO] ${item.name}` 
+    }
+    else if (item.enhancement === 18){
+        item.displayName = `[TRI] ${item.name}` 
+    }
+    else if (item.enhancement === 19){
+        item.displayName = `[TET] ${item.name}` 
+    }
+    else if (item.enhancement === 20){
+        item.displayName = `[PEN] ${item.name}` 
+    };
+    return item;
 }
 
 function fail(item) {
-    if (item.level < 15){
-        item.level = item.level - 1;
-        item.durability = item.durability - 5;
-        item.displayName = `[+${item.level}] ${item.name}`;
-        return item;
+    item.enhancement = item.enhancement - 1;
+    if (item.enhancement < 15){
+        if (item.durability > 25) {
+            item.durability = item.durability - 5;
+        }
+        else {
+            item.durability = 20;
+        }
+        item.displayName = `[+${item.enhancement}] ${item.name}`;
     }
+    else {
+        if (item.durability > 10) {
+            item.durability = item.durability - 10;
+        }
+        else {
+            item.durability = 0;
+        };
+        if (item.enhancement === 16){
+           item.displayName = `[PRI] ${item.name}` 
+        }
+        else if (item.enhancement === 17){
+            item.displayName = `[DUO] ${item.name}` 
+         }
+        else if (item.enhancement === 18){
+            item.displayName = `[TRI] ${item.name}` 
+         }
+        else if (item.enhancement === 19){
+            item.displayName = `[TET] ${item.name}` 
+        };
+    }
+    return item;
 }
 
 function repair(item) {
--- items/enhancer.spec.js
+++ items/enhancer.spec.js
@@ -2,39 +2,103 @@ const {success, fail, repair} = require('./enhancer.js');
 
 const testItem1 = {
     name: 'Scimitar of Tedium',
-    level: 4,
+    enhancement: 4,
     type: 'weapon',
-    displayName: `[+${this.level}] ${this.name}`,
+    displayName: `[+${this.enhancement}] ${this.name}`,
     durability: 100
 }
 
 const testItem2 = {
     name: 'Scimitar of Tedium',
-    level: 10,
+    enhancement: 10,
     type: 'weapon',
-    displayName: `[+${this.level}] ${this.name}`,
+    displayName: `[+${this.enhancement}] ${this.name}`,
+    durability: 100
+}
+
+const testItem3 = {
+    name: 'Scimitar of Tedium',
+    enhancement: 19,
+    type: 'weapon',
+    displayName: `[TET] ${this.name}`,
+    durability: 100
+}
+
+const testItem4 = {
+    name: 'Scimitar of Tedium',
+    enhancement: 19,
+    type: 'weapon',
+    displayName: `[TET] ${this.name}`,
+    durability: 9
+}
+
+const testItem5 = {
+    name: 'Scimitar of Tedium',
+    enhancement: 15,
+    type: 'weapon',
+    displayName: `${this.enhancement} ${this.name}`,
+    durability: 24
+}
+
+const testItem6 = {
+    name: 'Scimitar of Tedium',
+    enhancement: 15,
+    type: 'weapon',
+    displayName: `${this.enhancement} ${this.name}`,
     durability: 100
 }
 
 const successTest1 = {
     name: 'Scimitar of Tedium',
-    level: 5,
+    enhancement: 5,
     type: 'weapon',
     displayName: `[+5] Scimitar of Tedium`,
     durability: 100
 }
 
+const successTest2 = {
+    name: 'Scimitar of Tedium',
+    enhancement: 20,
+    type: 'weapon',
+    displayName: `[PEN] Scimitar of Tedium`,
+    durability: 100
+}
+
+const successTest3 = {
+    name: 'Scimitar of Tedium',
+    enhancement: 16,
+    type: 'weapon',
+    displayName: `[PRI] Scimitar of Tedium`,
+    durability: 100
+}
+
 const failTest1 = {
     name: 'Scimitar of Tedium',
-    level: 9,
+    enhancement: 9,
     type: 'weapon',
     displayName: `[+9] Scimitar of Tedium`,
     durability: 95
 }
 
+const failTest2 = {
+    name: 'Scimitar of Tedium',
+    enhancement: 18,
+    type: 'weapon',
+    displayName: `[TRI] Scimitar of Tedium`,
+    durability: 0
+}
+
+const failTest3 = {
+    name: 'Scimitar of Tedium',
+    enhancement: 14,
+    type: 'weapon',
+    displayName: `[+14] Scimitar of Tedium`,
+    durability: 20
+}
+
 const repairTest1 = {
     name: 'Scimitar of Tedium',
-    level: 9,
+    enhancement: 9,
     type: 'weapon',
     displayName: `[+9] Scimitar of Tedium`,
     durability: 100
@@ -44,12 +108,16 @@ describe('enhancer.js', () => {
     describe('success()', () => {
         test('upgrades item', () =>{
             expect(success(testItem1)).toEqual(successTest1);
+            expect(success(testItem3)).toEqual(successTest2);
+            expect(success(testItem6)).toEqual(successTest3);
         });
     });
 
     describe('fail()', () => {
         test('downgrades item', () => {
             expect(fail(testItem2)).toEqual(failTest1);
+            expect(fail(testItem4)).toEqual(failTest2);
+            expect(fail(testItem5)).toEqual(failTest3);
         });
     });
 
